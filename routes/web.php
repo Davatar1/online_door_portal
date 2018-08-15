ad404f16c823f1be4a41cde368fffb09fe2b70b3
Usman Mohammed <usmanmohammed1997@gmail.com>
2018-08-15 11:24:04 -0400

    Added Nav Bar

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin',function(){
+ return('adminhome');
+});
