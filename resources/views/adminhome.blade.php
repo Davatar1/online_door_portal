acabe49fece6f9fc976d533d31904aa9d1dc6182
Usman Mohammed <usmanmohammed1997@gmail.com>
2018-08-15 17:02:15 -0400

    Added the admin homepPage

---
+++ resources/views/adminhome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.admin')
+    @section('content')
+    <div class= "content">
+        <div class="title m-b-md">
+            <h1>It is a start!</h1>
+        </div>
+    </div>
+    @endsection
