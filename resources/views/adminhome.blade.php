4ddc95fc4bada99af704f4be0def5987788bebf4
Davatar1 <fusrodah1000@gmail.com>
2018-09-04 08:14:29 -0500

    edited admin home added some code for the table

--- resources/views/adminhome.blade.php
+++ resources/views/adminhome.blade.php
@@ -7,5 +7,15 @@
         <div class="title m-b-md">
             <h1>Start!</h1>
         </div>
+        <div class="table">
+ <table>
+    @foreach($Doors as $Door)
+    <tr><td>{{$Door->name}} - {{$Door->created_at}}</td></tr>
+</table>
+    @endforeach
+        </div>
+
+
+
     </div>
     @endsection
